5a2b3c897664c69aecccd050391f0977b9542534
vladNew91 <vladnew91@gmail.com>
2020-10-15 22:08:40 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let a = bracketsConfig.map((e) => e.join(""));
+    for (let i = 0; i < a.length; i++) {
+        if (str.includes(a[i])) {
+            str = str.replace(a[i], "");
+            i = -1;
+        }
+    }
+    if (str.length == 0) {
+        return true;
+    } else return false;
+};
